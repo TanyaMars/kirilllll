dfdc6eac65273ad7725ba0b4479f1c2b9de54e1a
mazik <tanayny18@gmail.com>
2024-11-28 20:05:27 +0300

    First commit(1)

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         // = != ==
         Scanner in = new Scanner(System.in);
+
+
+
+
         int num1 = in.nextInt();
         int num2 = in.nextInt();
 
