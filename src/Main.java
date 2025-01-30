14f7734c20711933d5874fb8d3e1a23e5f07baa9
mazik <tanayny18@gmail.com>
2025-01-30 18:47:56 +0300

    Create calculate(method)

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // = != ==
-        Scanner in = new Scanner(System.in);
-        //OMG
+        calculate();
+    }
 
+    public static void calculate(){
+            Scanner in = new Scanner(System.in);
 
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
 
-        System.out.println("Выберите действие: \n 1. * \n 2. + \n 3. - \n 4. / ");
-        int num = in.nextInt();
-        if (num == 1){
-            System.out.println(num1 * num2);
-        }
-        else if (num == 2) {
-            System.out.println(num1 + num2);
-        }
-        else if (num == 3){
-            System.out.println(num1 - num2);
-        }
-        else if (num == 4){
-            System.out.println(num1 / num2);
-        }
-        else{
-            System.out.println("Ну ты и дурак");
-        }
+            System.out.println("Выберите действие: \n 1. * \n 2. + \n 3. - \n 4. / ");
+            int num = in.nextInt();
+            if (num == 1){
+                System.out.println(num1 * num2);
+            }
+            else if (num == 2) {
+                System.out.println(num1 + num2);
+            }
+            else if (num == 3){
+                System.out.println(num1 - num2);
+            }
+            else if (num == 4){
+                System.out.println(num1 / num2);
+            }
+            else{
+                System.out.println("Ну ты и дурак");
+            }
     }
 }
+
