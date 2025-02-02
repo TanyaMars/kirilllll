cc951a1bfe8d4002d0240a2e3f45ef192ac43057
mazik <tanayny18@gmail.com>
2025-02-02 23:54:20 +0300

    Game - Guess the number

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,7 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // = != ==
-        calculate();
+        int d = diap();
+        number_guessing(d);
     }
 
     public static void calculate(){
@@ -32,5 +34,48 @@ public class Main {
                 System.out.println("Ну ты и дурак");
             }
     }
+
+    public static int diap(){
+        System.out.println("Choose the diapason: \n 1. 1-100 \n 2. 1-1000 \n 3. 1-10000");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        switch(num){
+            case 1:
+                int r1 = (int) (Math.random()*100) + 1;
+                return r1;
+            case 2:
+                int r2 = (int) (Math.random()*1000) + 1;
+                return r2;
+            case 3:
+                int r3 = (int) (Math.random()*10000) + 1;
+                return r3;
+            default:
+                System.out.println("Incorrect number");
+                diap();
+                break;
+        }
+        return diap();
+    }
+
+    public static void number_guessing(int num){
+        // Радомное число от 1 до 100 и число попыток 5
+        System.out.println("Guess the number");
+        Scanner in = new Scanner(System.in);
+        int in_num = in.nextInt();
+        int c = 1;
+        while (in_num != num){
+            if (in_num<num){
+                System.out.println("Make it bigger");
+            }
+            else if (in_num > num) {
+                System.out.println("Make it smaller");
+            }
+            in = new Scanner(System.in);
+            in_num = in.nextInt();
+            c ++;
+        }
+        System.out.println("Winner \n Count of try = " + c);
+    }
 }
+
 
